317a8fbe958f81433966af1ddb69d8e12ebd33d0
Alex LaFroscia <alex@lafroscia.com>
2024-03-08 21:11:19 -0500

    update memoization technique

--- src/lib/wikipedia.ts
+++ src/lib/wikipedia.ts
@@ -1,45 +1,14 @@
 import wiki, { type Page, type imageResult, type wikiSummary } from 'wikipedia';
+import { memoize } from './fn';
 
-const PAGE_CACHE = new Map<string, Page>();
-const IMAGES_CACHE = new WeakMap<Page, imageResult[]>();
-const SUMMARY_CACHE = new WeakMap<Page, wikiSummary>();
+export const page = memoize(function (name: string): Promise<Page> {
+	return wiki.page(name);
+});
 
-export async function page(name: string): Promise<Page> {
-	if (PAGE_CACHE.has(name)) {
-		return PAGE_CACHE.get(name)!;
-	}
+export const images = memoize(function (page: Page): Promise<imageResult[]> {
+	return page.images();
+});
 
-	const result = await wiki.page(name);
-
-	PAGE_CACHE.set(name, result);
-
-	return result;
-}
-
-export async function images(page: Page): Promise<imageResult[]> {
-	if (!page) {
-		return [];
-	}
-
-	if (IMAGES_CACHE.has(page)) {
-		return IMAGES_CACHE.get(page)!;
-	}
-
-	const results = await page.images();
-
-	IMAGES_CACHE.set(page, results);
-
-	return results;
-}
-
-export async function summary(page: Page): Promise<wikiSummary> {
-	if (SUMMARY_CACHE.has(page)) {
-		return SUMMARY_CACHE.get(page)!;
-	}
-
-	const result = await page.summary();
-
-	SUMMARY_CACHE.set(page, result);
-
-	return result;
-}
+export const summary = memoize(function (page: Page): Promise<wikiSummary> {
+	return page.summary();
+});
